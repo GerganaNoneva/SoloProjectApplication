1534d925afe2af15d7a03d2a540b5727efe2f88d
GerganaNoneva <gergananoneva@gmail.com>
2023-02-07 20:58:46 +0200

    New Header

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AvatarImage from 'public/images/avatar.png'
+
+export default function Avatar ({size=90, url, verified = false, }) {
+
+    return (
+        <div>
+            
+            
+    )
+
+
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import classNames from "classnames";
-import styles from "./header.module.scss";
+import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Logo from '../logo/Logo'
 import SearchIcon from '@mui/icons-material/Search'
-import { InputBase } from "@mui/material";
+import { ButtonBase, InputBase } from "@mui/material";
 import { Grade } from "@mui/icons-material";
 import Button from '@mui/material/Button'
 
@@ -13,12 +13,12 @@ import Button from '@mui/material/Button'
 export default function Header() {
 
     return (
-        <Container className={classNames(styles.container)} maxWidth="x12">
-            <Grid container spacing={1} columnSpacing={{ xs: 4, sm: 4, md: 6 }}  className={classNames(styles.grid)}>
-                <Grid item xs>
+        <Container className={classNames(styles.container)} maxWidth="x1">
+            <Grid container spacing={1}  columnSpacing={10} className={classNames(styles.grid)}>
+                <Grid item xs={2}>
                     <Logo> </Logo>
                 </Grid>
-                <Grid item xs={6} >
+                <Grid item xs={4}  >
                     <div className={classNames(styles.searchField)}>
                         <div>
                             <SearchIcon />
@@ -30,14 +30,12 @@ export default function Header() {
                         />
                     </div>
                 </Grid>
-                <Grid item xs>
-                    <div className={classNames(styles.links)}>
-                        <div> Home</div>
-                        <div> Activity </div>
-                        <div> 
-                        <Button>EXPLORE</Button>
-                        </div>
-                    </div>
+                <Grid item xs={4}>
+                    <Grid container spacing={1}  columnSpacing={6} className={classNames(styles.buttonContainer)}>
+                        <Grid item xs={4}><ButtonBase className={classNames(styles.link)}>Home</ButtonBase></Grid>
+                        <Grid item xs={4}> <ButtonBase className={classNames(styles.link)}>Activity</ButtonBase></Grid>
+                         <Grid item xs={4}> <Button className={classNames(styles.btn)}>EXPLORE</Button></Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
