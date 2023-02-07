b38c081983f43c60c436e1a46381f315e79c9061
GerganaNoneva <gergananoneva@gmail.com>
2023-02-07 21:38:08 +0200

    Add Avatar

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import AvatarImage from 'public/images/avatar.png'
-import classNames from "classnames";
-import styles from "./Avatar.module.scss";
-
-export default function Avatar ({size=90, url, verified = false, }) {
-
-    return (
-        <div className={classNames(styles.avatar)}>
-            <img src={url} className={classNames(styles.image)}>
-                <svg src=
-            </img>
-        </div>
-            
-            
-    )
+import styles from './Avatar.module.scss';
 
 
-}
+const Avatar = ({ size = 90, url, verified = false }) => {
+  return (
+    <div className={styles.avatar} style={{ width: size, height: size }}>
+      <img className={styles.image} src={url} alt="avatar" style={{ width: size }} />
+      {verified && <img className={styles.badge} src='./images/verified.svg' alt="verified" />}
+    </div>
+  );
+};
+
+export default Avatar;
