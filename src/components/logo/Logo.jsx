b88788c2b5e6661ed5a653f82f14c603283e0d7c
GerganaNoneva <gergananoneva@gmail.com>
2023-02-07 11:52:29 +0200

    Expodrt default Logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Logo =({type='default'}) => {
-    return (
-        <Logo>
-            {type === 'default' && <img src='./images/logo.svg' alt="Default logo"></img>} 
-            {type ==='muted' && <img src='./images/logo-muted.svg' alt="Muted logo"></img>}
-        </Logo>
-    )
-}
+export default function Logo ({type='default'}) {
+    let logo;
+    if (type==='default') {
+        logo = <img src='./images/logo.svg' alt="Default logo"/>;
+    } else if (type ==='muted') {
+        logo = <img src='./images/logo-muted.svg' alt="Muted logo"/>;
+    }
+    return logo;
+};
 
-export {Logo};
